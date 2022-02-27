c1ae75c5be1a427718f35ce1f3507b3f0fac47ab
Jorge Junior <jjorge98@outlook.com>
2022-02-27 06:30:18 -0300

    Data Array Done / Flatlist Done / Home Done

---
+++ src/components/CustomFlatlist.js
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class CustomFlatlist extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderItem = ({ item }, navigation) => {
+    return (
+      <TouchableOpacity
+        style={styles.listItem}
+        onPress={() => navigation.navigate('Profile', { character: item })}>
+        <View style={styles.imgArea}>
+          <Image style={styles.img} source={item.icon} />
+        </View>
+        <View style={styles.textArea}>
+          <Text style={styles.title}>{item.name}</Text>
+          <Text style={styles.subTitle}>
+            Apelido(s): {item.aliases.length ? item.aliases : 'Não possui'}
+          </Text>
+          <Text style={styles.subTitle}>
+            Codinome(s): {item.codename.length ? item.codename : 'Não possui'}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    const { data, navigation } = this.props;
+
+    return (
+      <FlatList
+        data={data}
+        renderItem={item => this.renderItem(item, navigation)}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    marginTop: 15,
+    backgroundColor: '#ffffff',
+    height: 100,
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    shadowColor: '#000000',
+    shadowOpacity: 0.8,
+    shadowRadius: 10,
+    elevation: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#0a1b1f',
+  },
+  imgArea: {
+    flex: 2,
+    marginStart: 15,
+  },
+  textArea: {
+    flex: 5,
+  },
+  img: {
+    height: '80%',
+    width: '80%',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#0a1b1f',
+  },
+  subTitle: {
+    fontSize: 15,
+    color: '#0a1b1f',
+  },
+});
---
+++ src/components/CustomHeader.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default class CustomHeader extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { title, description } = this.props;
+    return (
+      <View>
+        <Image
+          style={styles.img}
+          source={require('../assets/images/valorantLogo.png')}
+        />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  img: {
+    height: 120,
+    width: 120,
+    alignSelf: 'center',
+  },
+  title: {
+    marginTop: 20,
+    fontSize: 25,
+    fontFamily: 'fantasy',
+    alignSelf: 'center',
+    color: '#0a1b1f',
+    fontWeight: '600',
+  },
+  description: {
+    marginTop: 20,
+    fontSize: 18,
+    fontFamily: 'fantasy',
+    color: '#ffffff',
+    justifyContent: 'center',
+    textAlign: 'center',
+    paddingHorizontal: 30,
+  },
+});
